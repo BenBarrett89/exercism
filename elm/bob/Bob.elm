bf5d985136800baa9138dc9f3f6da673f4d37911
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-08 12:04:33 +0000

    Started Bob

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,1 +1,32 @@
 module Bob exposing (..)
+
+import String
+import Char
+import Regex
+
+hey: String -> String
+hey talk =
+  if yelling talk then
+    "Whoa, chill out!"
+  else if question talk then
+    "Sure."
+  else if silence talk then
+    "Fine. Be that way!"
+  else
+    "Whatever."
+
+question: String -> Bool
+question talk =
+  String.endsWith "?" talk
+
+yelling: String -> Bool
+yelling talk =
+  String.all uppercase talk
+
+uppercase: Char -> Bool
+uppercase char =
+  Char.isUpper char
+
+silence: String -> Bool
+silence talk =
+  False
