90ac5dfbf5ccb345fda242bb4d0964152e7a8b91
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-09 11:50:23 +0000

    Bob

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,35 @@
-(ns bob)
+(ns bob
+  (:require [clojure.string :as str]))
 
-(defn response-for [] ;; <- arglist goes here
-  ;; your code goes here
+(defn containsLetters [talk]
+  (re-find (re-matcher #"[a-zA-Z]+" talk))
+)
+
+(defn lettersAllUppercase [talk]
+  (= (str/upper-case talk) talk)
+)
+
+(defn shouting [talk]
+  (and (containsLetters talk) (lettersAllUppercase talk))
+)
+
+(defn question [talk]
+  (str/ends-with? talk "?")
+)
+
+(defn silence [talk]
+  (str/blank? talk)
+)
+
+(defn response-for [talk]
+  (if (silence talk)
+    "Fine. Be that way!"
+    (if (shouting talk)
+      "Whoa, chill out!"
+      (if (question talk)
+        "Sure."
+        "Whatever."
+      )
+    )
+  )
 )
