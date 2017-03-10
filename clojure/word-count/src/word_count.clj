e8b8cc3614f1a5429e01f4398cd68b2cac2f4984
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-10 10:48:11 +0000

    Word Count 2

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,23 +1,10 @@
 (ns word-count
-  (:require [clojure.string :as str]))
+  (:use [clojure.string :only [lower-case]]))
 
 (defn to-words [sentence]
-  (str/split sentence #" ")
-)
-
-(defn prepare-word [word]
-  (re-find (re-matcher #"[a-z0-9]+" (str/lower-case word)))
-)
-
-(defn count-words [words raw-word]
-  (def word (prepare-word raw-word))
-  (cond
-    (nil? word) words
-    (contains? words word) (assoc words word (+ (get words word) 1))
-    :else (assoc words word 1)
-  )
+  (map lower-case (re-seq #"\w+" sentence))
 )
 
 (defn word-count [sentence]
-  (reduce count-words {} (to-words sentence))
+  (->> sentence (to-words) (frequencies))
 )
