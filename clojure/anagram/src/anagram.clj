60ee5187f989a0bb586c65b8a11291c33d89f935
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-13 11:11:10 +0000

    Anagram

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,5 +1,26 @@
-(ns anagram)
+(ns anagram
+  (:use [clojure.string :only [lower-case split]])
+)
 
-(defn anagrams-for [] ;; <- arglist goes here
-  ;; your code goes here
+(defn split-to-sorted-letters [word]
+  (sort (split (lower-case word) #""))
+)
+
+(defn not-same-word [word potential-anagram]
+  (not= (lower-case word) (lower-case potential-anagram))
+)
+
+(defn same-letters [word potential-anagram]
+  (= (split-to-sorted-letters word) (split-to-sorted-letters potential-anagram))
+)
+
+(defn anagram-of [word potential-anagram]
+    (and
+      (not-same-word word potential-anagram)
+      (same-letters word potential-anagram)
+    )
+)
+
+(defn anagrams-for [word potential-anagrams]
+  (filter #(anagram-of word %) potential-anagrams)
 )
