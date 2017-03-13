55617d2c7c1829930e54a25bced22d2f78bab715
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-13 14:43:00 +0000

    Bob

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -11,18 +11,14 @@ hey talk =
   else if question talk then "Sure."
   else "Whatever."
 
-question: String -> Bool
-question talk =
-  String.endsWith "?" talk
-
-yelling: String -> Bool
-yelling talk =
-  String.all uppercase talk && Regex.contains (Regex.regex "[a-zA-Z]+") talk
-
-uppercase: Char -> Bool
-uppercase char =
-  Char.isUpper char
-
 silence: String -> Bool
 silence talk =
   (String.isEmpty (String.trim talk))
+
+yelling: String -> Bool
+yelling talk =
+  (String.toUpper talk == talk) && not (String.toLower talk == talk)
+
+question: String -> Bool
+question talk =
+  String.endsWith "?" talk
