5a46c2a74d69df987c18e90ce896918c987bebc8
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-15 09:56:04 +0000

    Accumulate

--- elm/accumulate/Accumulate.elm
+++ elm/accumulate/Accumulate.elm
@@ -1,1 +1,11 @@
 module Accumulate exposing (..)
+
+import List exposing (head, tail)
+
+accumulate: (a -> b) -> List a -> List b
+accumulate operation collection =
+  case (head collection) of
+    Nothing -> []
+    Just item -> case (tail collection) of
+      Nothing -> [(operation item)]
+      Just listTail -> [(operation item)] ++ (accumulate operation listTail)
