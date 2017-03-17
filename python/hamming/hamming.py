bfb307d6f2a08045ac4b10da416d108f2edb91d9
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-17 10:59:39 +0000

    Hamming 2

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,6 +2,6 @@ def distance(strandA, strandB):
     if len(strandA) != len(strandB):
         raise ValueError('DNA strand lengths are not equal')
     distance = 0
-    for i in range(0, len(strandA)):
-        if (strandA[i] != strandB[i]): distance += 1
+    for a, b in zip(strandA, strandB):
+        if (a != b): distance += 1
     return distance
