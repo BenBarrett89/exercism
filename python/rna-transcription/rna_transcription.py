02360e5407667b72b98dd4bca4ca5dfbb1b9d8f8
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-17 09:47:59 +0000

    RNA Transcription

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,12 @@
-def to_rna():
-    pass
+import re
+
+DNA = 'GCTA'
+RNA = 'CGAU'
+INVALID_DNA_NUCLEOTIDE = re.compile('[^'+DNA+']+')
+DNA_TO_RNA = str.maketrans(DNA, RNA)
+
+def to_rna(dnaStrand):
+    return dnaStrand.translate(DNA_TO_RNA) if valid_dna(dnaStrand) else ''
+
+def valid_dna(dnaStrand):
+    return not re.search(INVALID_DNA_NUCLEOTIDE, dnaStrand)
