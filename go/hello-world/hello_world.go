4ec0593559912dc460be566735a1352cbf98f6b5
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-20 15:40:04 +0000

    Hello World

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,9 @@
+// Package greeting implements a hello world function
+package greeting
+
+const testVersion = 4
+
+// HelloWorld says "Hello, World!"
+func HelloWorld() string {
+	return "Hello, World!"
+}
