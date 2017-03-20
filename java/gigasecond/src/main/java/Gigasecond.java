b542e48cf65cd683035464c1ab609178600bafac
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-20 13:00:26 +0000

    Gigasecond

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -1,19 +1,19 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-/**
- * Created by benjaminbarrett on 20/03/2017.
- */
 public class Gigasecond {
+    private static final long GIGASECOND_IN_SECONDS = 1000000000;
     private LocalDateTime date;
 
     public Gigasecond(LocalDate localDate) {
+        this(LocalDateTime.of(localDate.getYear(), localDate.getMonth(), localDate.getDayOfMonth(), 0, 0, 0));
     }
 
     public Gigasecond(LocalDateTime localDateTime) {
+        this.date = localDateTime.plusSeconds(GIGASECOND_IN_SECONDS);
     }
 
     public LocalDateTime getDate() {
-        return null;
+        return date;
     }
 }
