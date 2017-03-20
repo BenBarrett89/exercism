8cd4f47fc6839770746bcc03e69c49fa2e7800c3
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-20 16:13:13 +0000

    Leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -5,6 +5,11 @@ package leap
 const testVersion = 3
 
 // IsLeapYear returns whether a given year is a leap year or not
-func IsLeapYear(int) bool {
-	return false
+func IsLeapYear(year int) bool {
+	return isEvenlyDivisible(year, 4) && !(isEvenlyDivisible(year, 100) && !isEvenlyDivisible(year, 400))
+}
+
+// isEvenlyDivisible returns whether a given number has no remainder when divided by a given divisor
+func isEvenlyDivisible(number int, divisor int) bool {
+	return number%divisor == 0
 }
