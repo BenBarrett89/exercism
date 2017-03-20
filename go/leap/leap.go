3450559d2613992c17aa36ececef5b8b3569f07e
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-20 15:57:40 +0000

    Leap Start

---
+++ go/leap/leap.go
@@ -0,0 +1,10 @@
+// Package leap implements a function to calculate
+// whether a given year is a leap year
+package leap
+
+const testVersion = 3
+
+// IsLeapYear returns whether a given year is a leap year or not
+func IsLeapYear(int) bool {
+	return false
+}
