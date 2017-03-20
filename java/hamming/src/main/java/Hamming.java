d84f15e7893cbeb440f13b72045739c811c032a3
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-20 12:18:59 +0000

    Hamming

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,5 +1,13 @@
 public class Hamming {
     static int compute(String strandA, String strandB) {
-        return 0;
+        int hammingDistance = 0;
+        if (strandA.length() != strandB.length()) {
+            throw new IllegalArgumentException("Strand lengths must be equal to calculate Hamming distance");
+        }
+        for (int i = 0; i < strandA.length(); i++) {
+            int characterDistance = Character.compare(strandA.charAt(i), strandB.charAt(i));
+            hammingDistance += (characterDistance != 0) ? 1 : 0;
+        }
+        return hammingDistance;
     }
 }
