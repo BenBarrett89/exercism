2b562210dbc4437eda026e7902c7477a54748b34
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-21 10:51:02 +0000

    Hamming Start

---
+++ go/hamming/hamming.go
@@ -0,0 +1,9 @@
+// Package hamming implements a function to calculate the Hamming distance
+package hamming
+
+const testVersion = 5
+
+// Distance function returns the Hamming distance between two given strands
+func Distance(a, b string) (int, error) {
+	return 0, nil
+}
