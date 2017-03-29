b4f6a744891e8ea80691c83bc27440f2c6b10213
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-29 15:43:09 +0100

    Hamming 2
    Made shorter by using errors

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,24 +1,14 @@
 // Package hamming implements a function to calculate the Hamming distance
 package hamming
 
-import "fmt"
+import "errors"
 
 const testVersion = 5
-
-type ErrStrandLengthsNotEqual struct {
-	strandALength int
-	strandBLength int
-}
-
-func (e ErrStrandLengthsNotEqual) Error() string {
-	return fmt.Sprintf("strand A length is %v, but strand B length is %v - strand lengths must be equal",
-		e.strandALength, e.strandBLength)
-}
 
 // Distance function returns the Hamming distance between two given strands
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
-		return -1, ErrStrandLengthsNotEqual{len(a), len(b)}
+		return -1, errors.New("strand lengths must be equal")
 	}
 	count := 0
 	for i := 0; i < len(a); i++ {
