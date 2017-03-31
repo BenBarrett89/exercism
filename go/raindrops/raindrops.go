591ad73afaeda1b195c9aefc0e173399bc38fa64
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-31 16:18:33 +0100

    Raindrops 2

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,7 +1,44 @@
+// Package raindrops converts a number to 'raindrop-speak'
 package raindrops
+
+import (
+	"bytes"
+	"fmt"
+)
 
 const testVersion = 2
 
-func Convert(int) string
+var raindropSpeak = []struct {
+	factor int
+	sound  string
+}{
+	{3, "Pling"},
+	{5, "Plang"},
+	{7, "Plong"},
+}
 
-// The test program has a benchmark too.  How fast does your Convert convert?
+// Convert function converts the given number to 'raindrop-speak'
+func Convert(number int) string {
+	var raindropSound bytes.Buffer
+	raindropSound = convertToFactorSounds(number)
+	if raindropSound.Len() == 0 {
+		raindropSound.WriteString(fmt.Sprintf("%d", number))
+	}
+	return raindropSound.String()
+}
+
+// convertToFactorSounds function appends factors sounds for relevant factors
+func convertToFactorSounds(number int) bytes.Buffer {
+	var factorSounds bytes.Buffer
+	for _, raindrop := range raindropSpeak {
+		if isFactorOf(raindrop.factor, number) {
+			factorSounds.WriteString(raindrop.sound)
+		}
+	}
+	return factorSounds
+}
+
+// isFactorOf function returns true if the given number has the given factor as a factor
+func isFactorOf(factor int, number int) bool {
+	return number%factor == 0
+}
