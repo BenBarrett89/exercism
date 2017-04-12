e90a3c830703c334bb0d6bd8dbcaa6d862dee71c
Ben Barrett <benjaminbarrett1989@gmail.com>
2017-04-12 09:31:58 +0100

    Word Count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,12 @@
-def word_count():
-    pass
+import re
+
+def word_count(sentence):
+    count = {}
+    words = re.split(r'[_\W]', sentence.lower())
+    for word in words:
+        if word.isalnum():
+            if word not in count.keys():
+                count[word] = 1
+            else:
+                count[word] = count[word] + 1
+    return count
