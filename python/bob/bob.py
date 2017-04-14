8605c97c1280e9d7b62a2f78ed9dcf8c03e34e55
Ben Barrett <benjaminbarrett1989@gmail.com>
2017-04-14 21:01:55 +0100

    Bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,24 @@
-def hey():
-    pass
+DEFAULT_RESPONSE = "Whatever."
+QUESTION_RESPONSE = "Sure."
+YELLING_RESPONSE = "Whoa, chill out!"
+SILENCE_RESPONSE = "Fine. Be that way!"
+
+def hey(utterance):
+    talk = utterance.strip()
+    response = DEFAULT_RESPONSE
+    if addressed_with_silence(talk):
+        response = SILENCE_RESPONSE
+    elif yelled_at(talk):
+        response = YELLING_RESPONSE
+    elif asked_question(talk):
+        response = QUESTION_RESPONSE
+    return response
+
+def asked_question(talk):
+    return talk[-1:] == "?"
+
+def yelled_at(talk):
+    return talk.isupper()
+
+def addressed_with_silence(talk):
+    return len(talk) == 0
