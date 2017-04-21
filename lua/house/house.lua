c13c8cfe38f7f8a41a7a24ed3bedefb6fb30a302
Ben Barrett <benjaminbarrett1989@gmail.com>
2017-04-21 16:56:01 +0100

    House 2

--- lua/house/house.lua
+++ lua/house/house.lua
@@ -1,38 +1,36 @@
 local house = {}
 
 local words = {
-  [1]={nounPhrase = "house", verb="Jack built"},
-  [2]={nounPhrase = "malt", verb = "lay in"},
-  [3]={nounPhrase = "rat", verb = "ate"},
-  [4]={nounPhrase = "cat", verb = "killed"},
-  [5]={nounPhrase = "dog", verb = "worried"},
-  [6]={nounPhrase = "cow with the crumpled horn", verb = "tossed"},
-  [7]={nounPhrase = "maiden all forlorn", verb = "milked"},
-  [8]={nounPhrase = "man all tattered and torn", verb = "kissed"},
-  [9]={nounPhrase = "priest all shaven and shorn", verb = "married"},
-  [10]={nounPhrase = "rooster that crowed in the morn", verb = "woke"},
-  [11]={nounPhrase = "farmer sowing his corn", verb = "kept"},
-  [12]={nounPhrase = "horse and the hound and the horn", verb = "belonged to"},
+  "the house that Jack built",
+  "the malt\nthat lay in",
+  "the rat\nthat ate",
+  "the cat\nthat killed",
+  "the dog\nthat worried",
+  "the cow with the crumpled horn\nthat tossed",
+  "the maiden all forlorn\nthat milked",
+  "the man all tattered and torn\nthat kissed",
+  "the priest all shaven and shorn\nthat married",
+  "the rooster that crowed in the morn\nthat woke",
+  "the farmer sowing his corn\nthat kept",
+  "the horse and the hound and the horn\nthat belonged to"
 }
 
 function house.embedNounPhrase (line)
-  local THAT = '\nthat '
-  if line == 1 then THAT = ' that ' end
   if line > 0 then
-    return ' the ' .. words[line].nounPhrase .. THAT .. words[line].verb .. house.embedNounPhrase(line - 1)
+    return " " .. words[line] .. house.embedNounPhrase(line - 1)
   else
-    return ''
+    return ""
   end
 end
 
 function house.verse (line)
-  return 'This is' .. house.embedNounPhrase(line) .. '.'
+  return "This is" .. house.embedNounPhrase(line) .. "."
 end
 
 function house.recite ()
-  local recitation = ''
-  for i=1,#words do recitation = recitation .. house.verse(i) .. '\n' end
-  return recitation:sub(0, #recitation - 1)
+  local recitation = {}
+  for i = 1, #words do table.insert(recitation, house.verse(i)) end
+  return table.concat(recitation, "\n")
 end
 
 return house
