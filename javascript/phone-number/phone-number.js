fb20a2afe55680bcfd633fe5cbfc609a490ae387
Ben Barrett <benjaminbarrett1989@gmail.com>
2017-04-26 21:31:39 +0100

    Phone Number .number

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,5 +1,24 @@
-module.exports = function (number) {
-  this.number = () => {
-    return 0
+const phoneNumberValid = number => {
+  return number.length === 10
+}
+
+const cleanPhoneNumber = rawNumber => {
+  'use strict'
+  let number = rawNumber.replace(/[^\d]/g, '')
+  if (number.length === 11 && number.startsWith(1)) {
+    number = number.slice(1, 11)
   }
+  return number
+}
+
+const processPhoneNumber = rawNumber => {
+  'use strict'
+  let number = cleanPhoneNumber(rawNumber)
+  if (!phoneNumberValid(number)) { number = '0000000000' }
+  return number
+}
+
+module.exports = function (phoneNumber) {
+  const number = processPhoneNumber(phoneNumber)
+  this.number = () => { return number }
 }
--- javascript/phone-number/phone-number.spec.js
+++ javascript/phone-number/phone-number.spec.js
@@ -6,32 +6,32 @@ describe('PhoneNumber()', function () {
     expect(phone.number()).toEqual('1234567890')
   })
 
-  xit('cleans numbers with dots', function () {
+  it('cleans numbers with dots', function () {
     var phone = new PhoneNumber('123.456.7890')
     expect(phone.number()).toEqual('1234567890')
   })
 
-  xit('cleans some other number with dots', function () {
+  it('cleans some other number with dots', function () {
     var phone = new PhoneNumber('555.456.7890')
     expect(phone.number()).toEqual('5554567890')
   })
 
-  xit('valid when 11 digits and first digit is 1', function () {
+  it('valid when 11 digits and first digit is 1', function () {
     var phone = new PhoneNumber('11234567890')
     expect(phone.number()).toEqual('1234567890')
   })
 
-  xit('invalid when 11 digits and the first digit is NOT 1', function () {
+  it('invalid when 11 digits and the first digit is NOT 1', function () {
     var phone = new PhoneNumber('2  1234567890')
     expect(phone.number()).toEqual('0000000000')
   })
 
-  xit('invalid when 12 digits', function () {
+  it('invalid when 12 digits', function () {
     var phone = new PhoneNumber('991234567890')
     expect(phone.number()).toEqual('0000000000')
   })
 
-  xit('invalid when 9 digits', function () {
+  it('invalid when 9 digits', function () {
     var phone = new PhoneNumber('123456789')
     expect(phone.number()).toEqual('0000000000')
   })
