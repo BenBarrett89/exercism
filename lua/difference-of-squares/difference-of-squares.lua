1dfd65351232881bb900d0ebe3700955ee49ad8e
Ben Barrett <benjaminbarrett1989@gmail.com>
2017-04-26 20:17:18 +0100

    Difference of Squares

--- lua/difference-of-squares/difference-of-squares.lua
+++ lua/difference-of-squares/difference-of-squares.lua
@@ -1,15 +1,23 @@
 local diff = {}
 
 function diff.square_of_sums (number)
-  return 0
+  local total = 0
+  for i=1,number do
+    total = total + i
+  end
+  return total * total
 end
 
 function diff.sum_of_squares (number)
-  return 0
+  local total = 0
+  for i=1,number do
+    total = total + (i * i)
+  end
+  return total
 end
 
 function diff.difference_of_squares (number)
-  return 0
+  return diff.square_of_sums(number) - diff.sum_of_squares(number)
 end
 
 return diff
