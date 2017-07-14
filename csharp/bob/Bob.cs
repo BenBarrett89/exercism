6b7e6b80f26f9468787c1138716f298b8077a8b7
Ben Barrett <benjaminbarrett1989@gmail.com>
2017-07-14 14:42:20 +0100

    Bob (Some Changes)

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -2,8 +2,38 @@ using System;
 
 public static class Bob
 {
+    private static string DEFAULT_RESPONSE = "Whatever.";
+    private static string QUESTION_RESPONSE = "Sure.";
+    private static string SILENCE_RESPONSE = "Fine. Be that way!";
+    private static string YELLING_RESPONSE = "Whoa, chill out!";
+
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string response = DEFAULT_RESPONSE;
+        if (IsQuestion(statement)) {
+          response = QUESTION_RESPONSE;
+        }
+        else if (IsSilence(statement)) {
+          response = SILENCE_RESPONSE;
+        }
+        else if (IsYelling(statement)) {
+          response = YELLING_RESPONSE;
+        }
+        return response;
+    }
+
+    private static bool IsQuestion(string statement)
+    {
+      return statement.EndsWith("?");
+    }
+
+    private static bool IsSilence(string statement)
+    {
+      return false;
+    }
+
+    private static bool IsYelling(string statement)
+    {
+      return statement.ToUpper() == statement;
     }
 }
