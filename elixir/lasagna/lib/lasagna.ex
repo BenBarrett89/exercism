6b028f43fa4684f419c309e471f4c9ff97ac2c34
BenBarrett89 <benjaminbarrett1989@gmail.com>
2023-02-12 14:46:07 +0000

    elixir/lasagna 2

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,20 +1,26 @@
 defmodule Lasagna do
+  @expected_time_in_oven 40
+  @preparation_time_per_layer 2
 
-  def expected_minutes_in_oven, do: 40
-  def alarm, do: "Ding!"
+  @spec expected_minutes_in_oven() :: integer()
+  def expected_minutes_in_oven, do: @expected_time_in_oven
 
-  defp preparation_time_per_layer, do: 2
-
+  @spec remaining_minutes_in_oven(elapsed_minutes :: integer()) :: integer()
   def remaining_minutes_in_oven(elapsed_minutes) do
-    expected_minutes_in_oven - elapsed_minutes
+    expected_minutes_in_oven() - elapsed_minutes
   end
 
+  @spec preparation_time_in_minutes(number_of_layers :: integer()) :: integer()
   def preparation_time_in_minutes(number_of_layers) do
-    preparation_time_per_layer * number_of_layers
+    @preparation_time_per_layer * number_of_layers
   end
 
+  @spec total_time_in_minutes(number_of_layers :: integer(), elapsed_minutes :: integer()) :: integer()
   def total_time_in_minutes(number_of_layers, elapsed_minutes) do
     preparation_time_in_minutes(number_of_layers) + elapsed_minutes
   end
+
+  @spec alarm() :: String.t()
+  def alarm, do: "Ding!"
 
 end
