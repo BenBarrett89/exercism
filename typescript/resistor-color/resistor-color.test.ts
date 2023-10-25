c66bb404c85e267d12a33172d418ab908c917454
Ben Barrett <benjaminbarrett1989@gmail.com>
2023-10-25 11:41:16 +0100

    typescript/resitor-color

---
+++ typescript/resistor-color/resistor-color.test.ts
@@ -0,0 +1,36 @@
+import { colorCode, COLORS } from './resistor-color'
+
+describe('color code', () => {
+  it('Black', () => {
+    expect(colorCode('black')).toEqual(0)
+  })
+
+  it('White', () => {
+    expect(colorCode('white')).toEqual(9)
+  })
+
+  it('Orange', () => {
+    expect(colorCode('orange')).toEqual(3)
+  })
+
+  it('Magenta', () => {
+    expect(colorCode('magenta')).toEqual(-1) // not a valid color
+  })
+})
+
+describe('Colors', () => {
+  it('returns all colors', () => {
+    expect(COLORS).toEqual([
+      'black',
+      'brown',
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'violet',
+      'grey',
+      'white',
+    ])
+  })
+})
