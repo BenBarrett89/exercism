edc3c6a6b92c548c397f0ca96a27e45ca9855a8e
Ben Barrett <benjaminbarrett1989@gmail.com>
2023-11-08 15:46:47 +0000

    javascript/isbn-verifier
    Second iteration

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -1,12 +1,11 @@
-export const isValid = (isbn) => {
-  if (!/^\d-?\d{3}-?\d{5}-?[\d|X]$/.test(isbn)) return false;
-  const total = isbn
-    .split("")
-    .filter((digit) => !/-/.test(digit))
-    .reduce((sum, digit, index) => {
-      const value = digit === "X" ? 10 : parseInt(digit);
-      const factor = 10 - index;
-      return sum + value * factor;
-    }, 0);
-  return total % 11 === 0;
-};
+export const isValid = (isbn) =>
+  /^\d-?\d{3}-?\d{5}-?[\d|X]$/.test(isbn) &&
+  isbn
+    .match(/(\d|X$)/g)
+    .reduce(
+      (sum, digit, index) =>
+        sum + (digit === "X" ? 10 : parseInt(digit)) * (10 - index),
+      0
+    ) %
+    11 ===
+    0;
